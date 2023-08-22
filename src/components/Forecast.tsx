ee067f690d9244ebccea144000f6465164ff0afb
Olesia Kissa <okissaw@gmail.com>
2023-08-22 21:13:16 +0200

    Extract forecast data to a separate component

---
+++ src/components/Forecast.tsx
@@ -0,0 +1,34 @@
+import { ForecastType, ForecastItemType } from '../types'
+import Degree from './Degree'
+import Sunlight from './Sunlight'
+
+const Forecast = ( { data }: ForecastType): JSX.Element => {
+  const [ today ] = data.list
+  return (
+    <article>
+        <section>
+            <h1>
+              <Degree temp={Math.round(today.main.temp)} />
+            </h1>
+        </section>
+
+        <section id="carrousel">
+          {data.list.map((item: ForecastItemType, index: number) => (
+            <div key={index} className='item'>
+              <p>{index === 0 ? 'Now' : `${new Date(item.dt * 1000).getHours()}:00`}</p>
+              <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} 
+                   alt={item.weather[0].description} />
+              <Degree temp={Math.round(item.main.temp)} />
+            </div>
+          ))}
+        </section>
+
+        {/* Additional tiles */}
+        <section className='grid' id='tiles'>
+          <Sunlight sunrise={data.sunrise} sunset={data.sunset} />
+        </section>
+    </article>
+  )
+}
+
+export default Forecast
