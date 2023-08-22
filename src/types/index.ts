cca9d5e5a8e08ec067e692ea8b4f4fbc9e458caf
Olesia Kissa <okissaw@gmail.com>
2023-08-22 21:12:16 +0200

    Add type ForecastItem

--- src/types/index.ts
+++ src/types/index.ts
@@ -50,3 +50,40 @@ export type ForecastType = {
     sunrise: number
     sunset: number
 }
+
+export type ForecastItemType = {
+  dt: number,
+  main: {
+      temp: number,
+      feels_like: number,
+      temp_min: number,
+      temp_max: number,
+      pressure: number,
+      sea_level: number,
+      grnd_level: number,
+      humidity: number,
+      temp_kf:  number
+    },
+  weather: [
+      {
+          id: number,
+          main: string,
+          description: string,
+          icon: string
+      }
+  ],
+  clouds: {
+      all: number
+  },
+  wind: {
+      speed: number,
+      deg: number,
+      gust: number
+  },
+  visibility: number,
+  pop: number,
+  sys: {
+      pod: string | null
+  },
+  dt_txt: string
+}
