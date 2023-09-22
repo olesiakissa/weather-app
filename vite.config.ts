3e12ce3e9e3073b76572b1db35f62c0594d7970c
Olesia Kissa <okissaw@gmail.com>
2023-09-22 16:27:40 +0200

    Update config and package.json for secure connection and tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types='vitest' />
+
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), basicSsl()],
+  test: {
+    globals: true,
+    setupFiles: ['./src/__tests__/setup.ts'],
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+});
