35782fef318285e7b01aa8b90b31fc2a4cf8e041
Olesia Kissa <okissaw@gmail.com>
2023-09-28 12:33:03 +0200

    Move location permission context

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,21 +1,18 @@
-import SearchInput from './SearchInput'
-import SearchButton from './buttons/SearchButton'
-import LocalWeatherButton from './buttons/LocalWeatherButton'
-import LocationPermissionContextProvider from '../../contexts/LocationPermissionContextProvider'
+import SearchInput from './SearchInput';
+import SearchButton from './buttons/SearchButton';
+import LocalWeatherButton from './buttons/LocalWeatherButton';
 
 const Search = (): JSX.Element => {
-	return (
-	<header  id='location-search' className='flex col'>
-		<section className='flex'>
-			<SearchInput />
-			<SearchButton />
-		</section>
+  return (
+    <header id='location-search' className='flex col'>
+      <section className='flex'>
+        <SearchInput />
+        <SearchButton />
+      </section>
 
-		<LocationPermissionContextProvider>
-			<LocalWeatherButton />
-		</LocationPermissionContextProvider>
-	</header>
-	)
-}
+      <LocalWeatherButton />
+    </header>
+  );
+};
 
-export default Search
+export default Search;
