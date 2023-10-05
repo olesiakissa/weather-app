4115e6ed06fab2ff0fa720e0275a046e965294ec
Olesia Kissa <okissaw@gmail.com>
2023-10-05 10:33:50 +0200

    Change hero heading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,35 @@
-import { useAppContext } from '../hooks/useAppContext'
-import Degree from './forecast/Degree'
-import Home from '../assets/house.png'
-import { ForecastType } from '../types'
+import { useAppContext } from '../hooks/useAppContext';
+import Degree from './forecast/Degree';
+import Home from '../assets/house.png';
+import { ForecastType, OptionType } from '../types';
 
 const Hero = (): JSX.Element => {
-  const { forecast } = useAppContext() as {
-    forecast: ForecastType | undefined
-  }
+  const { forecast, location } = useAppContext() as {
+    forecast: ForecastType | undefined;
+    location: OptionType | null;
+  };
 
   let today;
-  if (forecast)
-    [ today ] = forecast.list
+  if (forecast) [today] = forecast.list;
+
+  const headingText = `${forecast?.name}${
+    location && location.name ? `, ${location.name}` : ''
+  }${location && location.state ? `, ${location.state}` : ''}`;
 
   return (
     <section id='hero' className='flex col'>
-      <h1 id='hero-heading'>{forecast && forecast.name}</h1>
-      <Degree temp={Math.round(today!.main.temp)} id='degree'/>
+      <h1 id='hero-heading'>{headingText}</h1>
+      <Degree temp={Math.round(today!.main.temp)} id='degree' />
       <div className='flex col'>
-          <p id='description'>{today!.weather[0].description}</p>
-          <div>
-            H: <Degree temp={Math.round(today!.main.temp_max)} id='high'/> L: <Degree temp={Math.round(today!.main.temp_min)} id='low'/>
-          </div>
+        <p id='description'>{today!.weather[0].description}</p>
+        <div>
+          H: <Degree temp={Math.round(today!.main.temp_max)} id='high' /> L:{' '}
+          <Degree temp={Math.round(today!.main.temp_min)} id='low' />
+        </div>
       </div>
       <img src={Home} alt='house logo' />
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
